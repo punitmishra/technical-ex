76ef7db9ca8b94053c8c1045b9b9d1dce89c1167
Punit Mishra <pmishra@calliduscloud.com>
2016-07-26 12:31:13 -0700

    Creating a basic question/test

---
+++ automation/src/main/java/com/callidus/automation/PageBase.java
@@ -0,0 +1,10 @@
+package com.callidus.automation;
+
+/**
+ * PageBase -- Define the base page object here.
+ *
+ */
+public class PageBase 
+{
+    //This is base page object.
+}
---
+++ automation/src/test/java/com/callidus/automation/HomePageTest.java
@@ -0,0 +1,42 @@
+package com.callidus.automation;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class HomePageTest {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void TechEx() {
+    	
+    	//Go to calliduscloud home page.
+        wd.get("http://www.calliduscloud.com/");
+        
+        //Click on "PRODUCTS"
+        wd.findElement(By.linkText("PRODUCTS")).click();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
